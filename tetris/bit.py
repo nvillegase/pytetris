f16271d2b9adf78c8490610540e93943da5a6e6a
Nicolás <nicolas@unergy.io>
2023-09-18 11:51:23 -0500

    Minor format adjustment

--- tetris/bit.py
+++ tetris/bit.py
@@ -2,21 +2,14 @@ from colorama import Fore
 
 class Bit:
 
-
     SQ = "▓"
 
-
-    def __init__(
-            self,
-            color: Fore = Fore.BLACK
-        ):
+    def __init__(self, color: Fore = Fore.BLACK):
         self.color = color
-
 
     @property
     def empty(self):
-        return self.color == Fore.BLACK
-
+        return (self.color == Fore.BLACK)
 
     def __str__(self):
         return self.color + 2*Bit.SQ + Fore.RESET
